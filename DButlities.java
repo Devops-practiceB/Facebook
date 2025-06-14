e6d1b71000f47773ac40952907e2deb46a6bedde
jayapal <jayapalreddy001@gmail.com>
2025-06-14 08:39:19 +0530

    First commit

---
+++ DButlities.java
@@ -0,0 +1,3 @@
+this is first calss 
+an git is very importent for the acction any 
+jayapal is first student coming from devops
---
+++ stringutlis.java
@@ -0,0 +1,3 @@
+Hi every one we are new to Devops calss
+and we are accept any customixed language to write the devops code
+and we are not responsable to other any existing working 
